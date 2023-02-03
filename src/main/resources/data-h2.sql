801629baa4eb3bc3237b0c84b576e4b88edf7c1c
rsimeonov <ruslansimeonov@wps-nl.com>
2023-02-03 10:44:40 +0100

    Finished project and changed to gradle

--- src/main/resources/data-h2.sql
+++
@@ -1,2 +0,0 @@
-insert into book(id, title, description, release_year) values ('b5607d38-8fc1-43ef-b44e—34967083c80a',
-'Ikigai', 'The Japanese secret to long and happy life ', 2018);
--- src/main/resources/db/migration/V1__BOOK_SCHEMA.sql
+++ src/main/resources/db/migration/V1__BOOK_SCHEMA.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS book (
-    id BINARY(255) NOT NULL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+CREATE TABLE book(
+    id uuid NOT NULL PRIMARY KEY ,
+    title VARCHAR NOT NULL ,
+    description VARCHAR NOT NULL ,
     release_year INT NOT NULL
-);
+)
